ee783de63a9ca54b700767eb2e86c7a21016859f
Farhad Ramezanghorbani <farhad.ghorbani@gmail.com>
2019-04-18 12:13:51 -0400

    LSTM RNN DNN feature extraction

--- data_prep/extract_features.py
+++ data_prep/extract_features.py
@@ -85,6 +85,44 @@ def get_features_LSTM(file_path, genre):
     return (data, target)
 
 
+def get_part_features_LSTM(file_path, genre):
+
+    ts_length = 256
+    hop_length = 512
+    sr = 22050
+    skip_length = 30
+
+    tot_length = librosa.get_duration(filename=file_path, sr=sr) # get duration in sec 
+    n_samples = int(tot_length // 60)  # one sample every 60 second
+
+    if n_samples == 0: # songs less than 60
+        n_samples = 1
+
+    data = np.zeros((n_samples, ts_length, 33), dtype=np.float64)
+    target = np.zeros((n_samples, 1))
+
+    duration = ts_length * hop_length/sr + 0.5 # seconds
+    
+    for n in range(n_samples):
+        
+        offset = skip_length + (n * 60)
+        y, sr = librosa.load(file_path, sr=sr, offset=offset , duration=duration , mono=True)
+        
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+        spectral_cent = librosa.feature.spectral_centroid(y=y, sr=sr, hop_length=hop_length)
+        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=hop_length)
+        spectral_cont = librosa.feature.spectral_contrast(y=y, sr=sr, hop_length=hop_length)
+
+        if genre == 'prog': target[n] = 1
+        else: target[n] = 0
+
+        data[n, :, 0:13] = mfcc.T[0:ts_length, :]
+        data[n, :, 13:14] = spectral_cent.T[0:ts_length, :]
+        data[n, :, 14:26] = chroma_stft.T[0:ts_length, :]
+        data[n, :, 26:33] = spectral_cont.T[0:ts_length, :]
+    
+    return (data, target)
+
 
 # MPI initialize the world
 comm = MPI.COMM_WORLD
@@ -94,7 +132,7 @@ data = []
 for g in genres:
     # get file names scatter, wait
     if comm.rank == 0:
-        files = os.listdir(f'{LOC}/{g}')
+        files = os.listdir(f'{LOC}/{g}') 
         file_arr = np.array_split(files, comm.size)
     else:
         file_arr = None
@@ -104,7 +142,6 @@ for g in genres:
 
     # work on the genre, wait
     for filename in file_arr:
-        #index = np.where(file_arr==filename)[0][0] + 1
         index = file_arr.tolist().index(filename) + 1
 
         if LSTM == False:
@@ -113,20 +150,22 @@ for g in genres:
 
         elif LSTM == True:
             # to_append (np.array, int)
-            array, target = get_features_LSTM(f'{LOC}/{g}/{filename}', f'{g}')
-            to_append = [array, target, f' {filename.replace(" ", "")}']
+            try:
+                array, target = get_part_features_LSTM(f'{LOC}/{g}/{filename}', f'{g}')
+                to_append = [array, target, f' {filename.replace(" ", "")}']
+            except:
+                print(f'Error: Something is wrong with {filename}')
+                to_append = []
 
         data.append(to_append)
-
+        
         print(f'{g} song {index}/{len(file_arr)} processed by rank {comm.rank}')
 
     comm.Barrier()
 
 
-
 # gather data from all ranks to 0
 data = comm.gather(data, root=0) # list of data lists
-
 if comm.rank == 0:
 
     dt = time() - t0
@@ -145,7 +184,7 @@ if comm.rank == 0:
 
         np.save(f'_{sys.argv[1]}', out_data)
         # after loading data = np.array('data.npy')
-        # data[:,0] >> arrays
+        # stack or concat data[:,0] >> arrays
 
     elif LSTM == False:
         file = open(out_file, 'w', newline='')
